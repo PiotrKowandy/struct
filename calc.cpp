fa5d6fc2083a8410f58d182abc7be7a657de2ea4
PiotrKowandy <33897068+PiotrKowandy@users.noreply.github.com>
2017-11-22 12:46:50 +0100

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main (int argc, char * argv[]) {
+
+	if (argc != 4){
+		cerr<< "Liczba argumentow mniejsza od 4" << endl;
+		return 1;
+	}
+
+
+	double lhs = stod (argv[1]);
+	string op = argv[2];
+	double rhs = stod(argv[3]);
+
+	if (op == "+"){
+		cout << "results = " << lhs + rhs << endl;
+	} else if (op == "-"){
+		cout << "results = " << lhs - rhs << endl;
+	} else if (op == "x") {
+		cout << "results = " << pow(lhs, rhs) << endl;
+	} else {
+		cerr << "Error: op " << op << " not supported." << endl;
+		return 1;
+	}
+	
+
+
+	return 0;
+}
